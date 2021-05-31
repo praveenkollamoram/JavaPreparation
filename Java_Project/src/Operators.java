d697608647273a480fa5fd1817981d4cc7f5707a
Admin <Admin@DESKTOP-P3MI8JF>
2021-05-31 12:00:01 +0530

    2 commit

---
+++ Java_Project/src/Operators.java
@@ -0,0 +1,34 @@
+
+public class Operators 
+{
+	
+	
+/*
+ * Arithmetic operators
+ * Bitwise Operators
+ * Relational
+ * Logical
+ * 
+ * 
+ */
+	public static void main(String args[])
+	{
+		/*int r1 = m+n;
+		int r2 = m-n;
+		int r3 = m*n;
+		double r4 = (double)m/n; // to get quotient
+		
+		int r5 = m%n; // to get remainder
+		
+		
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println(r3);
+		System.out.println(r4);
+		System.out.println(r5);
+		 
+		 */
+		
+	}
+
+}
